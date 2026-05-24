d3a7639d4c4881964043cc092698fb80ed43fa93
cainapenagosanasofia <cainapenagosanasofia@gmail.com>
2026-05-24 21:35:43 +0000

    Agrega el metodo ONInit para llamar al objeto por ID para componentes de cat-detil y dog-detail, ademas de pantalla de objeto no encontrado en caso de ingresar mal la ruta

--- src/app/cat-detail/cat-detail.ts
+++ src/app/cat-detail/cat-detail.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, OnSameUrlNavigation, RouterLink } from '@angular/router';
+import { PetService, Pet } from '../services/pet';
 
 @Component({
   selector: 'app-cat-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cat-detail.html',
   styleUrl: './cat-detail.css',
 })
-export class CatDetail {}
+export class CatDetail implements OnInit {
+  private route = inject(ActivatedRoute);
+  private petService = inject(PetService);
+  cat: Pet | undefined;
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.cat = this.petService.getCatById(id);
+  }
+}
--- src/app/dog-detail/dog-detail.ts
+++ src/app/dog-detail/dog-detail.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit} from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { PetService, Pet } from '../services/pet';
 
 @Component({
   selector: 'app-dog-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './dog-detail.html',
   styleUrl: './dog-detail.css',
 })
-export class DogDetail {}
+export class DogDetail implements OnInit{
+  private route = inject(ActivatedRoute);
+  private petService = inject(PetService);
+  dog: Pet | undefined;
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.dog = this.petService.getDogById(id);
+  }
+}
